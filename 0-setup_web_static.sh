fb2785565de8683155884b649d59f5ef0bd5b3f7
julesineza <j.ineza@alustudent.com>
2026-02-09 12:54:26 +0200

    setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+if ! command -v nginx >/dev/null 2>&1; then
+sudo apt-get update -y
+sudo apt-get install nginx -y
+fi
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "working jules" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+CONFIG_FILE="/etc/nginx/sites-available/default"
+
+if ! grep -q "hbnb_static" "$CONFIG_FILE"; then
+sudo sed -i '/server {/a
+location /hbnb_static/ {
+alias /data/web_static/current/;
+}
+' "$CONFIG_FILE"
+fi
+
+sudo nginx -t && sudo systemctl restart nginx
