08fc990983a9d7e2ecda1f524bfbb16d0f4eb281
julesineza <j.ineza@alustudent.com>
2026-02-09 20:38:21 +0200

    1-pack

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""packing"""
+from fabric.api import local
+from datetime import datetime
+import os 
+
+def do_pack():
+    try:
+        if not os.path.isdir("versions"):
+
+            local("mkdir -p versions")
+
+            now = datetime.now()
+            archive_name = f"web_static_{now.year}{now.month}{now.day}{now.hour}{now.minute}{now.second}.tgz"
+
+            archive_path=f"/versions/{archive_name}"
+
+            result =local(f"tar -cvzf {archive_path} webstatic")
+
+        if result.succeeded:
+            archive_size = os.path.getsize(archive_path)
+            print("web_static packed: {} -> {}Bytes".format(
+                archive_path, archive_size))
+            return archive_path
+        else:
+            return None
+    except Exception :
+        return  None 
+
